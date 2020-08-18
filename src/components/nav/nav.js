c6c2868b8bec0fba59319648018d3fb4eac83c72
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-08-18 08:33:48 -0700

    Update nav.js

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import './nav-style.css';
+import { Link } from 'react-scroll';
 import logo from '../../assets/Group 1@2x.png';
 const Nav = () => (
 	<div className="navBar">
-    <div className="logo"> 
-    <img src={logo} alt="logo" />
-    <h1>Kidus Kinfe </h1>
-    </div>
+		<Link activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={500}>
+			<div className="logo">
+				<img src={logo} alt="logo" />
+				<h1>Kidus Kinfe </h1>
+			</div>
+		</Link>
 
-    <nav className="deskTopNav">
-    <ul>
-        <li>My Work</li>
-        <li>My Services</li>
-        <li>About Me</li>
-        <div> Talk 2 Me </div>
-    </ul>
+		<nav className="deskTopNav">
+			<ul>
+				<Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500}>
+					<li>My Work</li>
+				</Link>
 
-    
-    </nav>
+				<Link activeClass="active" to="services" spy={true} smooth={true} offset={-70} duration={500}>
+					<li>My Services</li>
+				</Link>
 
-    <nav className="moblieNav">
-    <ul>
-        <li>My Work <br/> <i className="material-icons">computer</i></li>
-        <li>My Services <br/><i className="material-icons">settings</i></li>
-        <li>About Me <br/><i className="material-icons">info_outline</i></li>
-        
-    </ul>
-     </nav>
-	
+				<Link activeClass="active" to="aboutMe" spy={true} smooth={true} offset={-70} duration={500}>
+					<li>About Me</li>
+				</Link>
 
+				<Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>
+					<div> Talk 2 Me </div>
+				</Link>
+			</ul>
+		</nav>
+
+		<nav className="moblieNav">
+			<ul>
+				<li>
+					My Work <br /> <i className="material-icons">computer</i>
+				</li>
+				<li>
+					My Services <br />
+					<i className="material-icons">settings</i>
+				</li>
+				<li>
+					About Me <br />
+					<i className="material-icons">info_outline</i>
+				</li>
+			</ul>
+		</nav>
 	</div>
 );
 
